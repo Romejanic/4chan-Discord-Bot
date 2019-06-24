e544e77c2231f7d398e88c17e5774c70d6524edc
Jack Davenport <romejanic@gmail.com>
2019-06-24 20:36:53 +1000

    Added Discord login

--- src/main.js
+++ src/main.js
@@ -1,21 +1,41 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-if(!fs.existsSync("config.json")) {
-	console.log("[ERROR] No config file found!");
-	console.log("[ERROR] Please edit the config.json file and add your bot token");
-
-	let inn = fs.createReadStream("data/config_default.json");
-	let out = fs.createWriteStream("config.json");
-	inn.pipe(out);
-	inn.on("end", () => {
-		process.exit();
-	});
-} else {
-	initBot();
-}
-
+// define functions
+ 
 function initBot() {
-	let config = require("config.json");
-	console.log(config.auth);
+	let config = require("../config.json");
+	if(!config || !config.auth || !config.auth.token) {
+		console.error("[ERROR] You must define a token! Please see data/config_default.json for a reference.");
+		return;
+	}
+
+	let client = new Discord.Client();
+	registerClientEvents(client);
+	client.login(config.auth.token);
 }
+
+function registerClientEvents(client) {
+	client.on("ready", () => {
+		console.log("[Client] Successfully logged in to Discord!");
+	});
+}
+
+function checkConfig(callback) {
+	if(!fs.existsSync("config.json")) {
+		console.error("[ERROR] No config file found!");
+		console.error("[ERROR] Please edit the config.json file and add your bot token");
+
+		let inn = fs.createReadStream("data/config_default.json");
+		let out = fs.createWriteStream("config.json");
+		inn.pipe(out);
+		inn.on("end", () => {
+			process.exit();
+		});
+	} else {
+		callback();
+	}
+}
+
+// bootstrap the bot
+checkConfig(initBot);

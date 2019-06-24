5374ffe0eb73196d69762e2faf50d86dee721511
Jack Davenport <romejanic@gmail.com>
2019-06-24 20:52:15 +1000

    [main.js] Added safe shutdown handler

--- src/main.js
+++ src/main.js
@@ -4,15 +4,39 @@ const fs = require("fs");
 // define functions
  
 function initBot() {
+	// check config
 	let config = require("../config.json");
 	if(!config || !config.auth || !config.auth.token) {
 		console.error("[ERROR] You must define a token! Please see data/config_default.json for a reference.");
 		return;
 	}
 
+	// create client
 	let client = new Discord.Client();
 	registerClientEvents(client);
 	client.login(config.auth.token);
+	console.log("[Client] Attempting login to Discord...");
+
+	// register safe exit
+	process.on("SIGINT", () => {
+		console.log("[Bot] Shutting down...");
+		client.destroy().then(() => {
+			console.log("[Client] Disconnected successfully!");
+		}, (err) => {
+			console.error("[Client] Failed to disconnect from Discord: " + err);
+		}).then(() => {
+			process.exit(); // kills the process
+		});
+	});
+	if(process.platform === "win32") { // workaround to detect SIGINT on Windows
+		let rl = require("readline").createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+		rl.on("SIGINT", () => {
+			process.emit("SIGINT");
+		});
+	}
 }
 
 function registerClientEvents(client) {
@@ -30,7 +54,7 @@ function checkConfig(callback) {
 		let out = fs.createWriteStream("config.json");
 		inn.pipe(out);
 		inn.on("end", () => {
-			process.exit();
+			process.exit(-1);
 		});
 	} else {
 		callback();

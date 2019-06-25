9bd4d2449947166f1370a995a21e58e5a0ba1de1
Jack Davenport <romejanic@gmail.com>
2019-06-25 12:51:51 +1000

    Put post generation into seperate function

--- src/4chan-api.js
+++ src/4chan-api.js
@@ -4,6 +4,17 @@ let apiUrl = "https://a.4cdn.org/{0}/{1}.json";
 let imgUrl = "https://i.4cdn.org/{0}/{1}{2}";
 let postUrl = "https://boards.4chan.org/{0}/thread/{1}";
 
+function getPostFromThread(thread) {
+    return {
+        image: imgUrl.format(board, thread.tim, thread.ext),
+        text: thread.com,
+        author: thread.name,
+        id: thread.no,
+        timestamp: thread.now,
+        permalink: postUrl.format(board, thread.no)
+    };
+}
+
 function getRandomPost(board) {
     return new Promise((resolve, reject) => {
         https.get(apiUrl.format(board, "catalog"), (res) => {
@@ -31,14 +42,7 @@ function getRandomPost(board) {
                 let threads = catalog[0].threads;
                 let thread = threads[Math.floor(Math.random() * threads.length)];
 
-                resolve({
-                    image: imgUrl.format(board, thread.tim, thread.ext),
-                    text: thread.com,
-                    author: thread.name,
-                    id: thread.no,
-                    timestamp: thread.now,
-                    permalink: postUrl.format(board, thread.no)
-                });
+                resolve(getPostFromThread(thread));
             });
         }).on("error", (err) => {
             reject(err);
